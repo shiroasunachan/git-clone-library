098ba5e65d91323f6fefd275e7e70f0a38a7e13e
shiroasunachan <49038994+shiroasunachan@users.noreply.github.com>
2019-04-03 23:36:11 +0700

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# git-clone-library
-kumpulan doang
---
+++ README.py
@@ -0,0 +1,91 @@
+# git-clone-library
+kumpulan doang
+
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 lazymux.py
+
+git clone https://github.com/maldevel/IPGeoLocation.git
+git clone https://github.com/maldevel/IPGeoLocation
+pip3 install -r requirements.txt --user
+
+Retrieve your IP Geolocation
+
+    ./ip2geolocation.py -m
+
+Retrieve IP Geolocation
+
+    ./ip2geolocation.py -t x.x.x.x
+
+Retrieve Domain Geolocation
+
+    ./ip2geolocation.py -t example.com
+
+Do not save .log files
+
+    ./ip2geolocation.py -t example.com --nolog
+
+Custom User Agent string
+
+    ./ip2geolocation.py -t x.x.x.x -u "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"
+
+Using Proxy
+
+    ./ip2geolocation.py -t x.x.x.x -x http://127.0.0.1:8080
+
+Using random Proxy
+
+    ./ip2geolocation.py -t x.x.x.x -X /path/to/proxies/filename.txt
+
+Pick User-Agent string randomly
+
+    ./ip2geolocation.py -t x.x.x.x -U /path/to/user/agent/strings/filename.txt
+
+Retrieve IP geolocation and open location in Google maps with default browser
+
+    ./ip2geolocation.py -t x.x.x.x -g
+
+Export results to CSV file
+
+    ./ip2geolocation.py -t x.x.x.x --csv /path/to/results.csv
+
+Export results to XML file
+
+    ./ip2geolocation.py -t x.x.x.x --xml /path/to/results.xml
+
+Export results to TXT file
+
+    ./ip2geolocation.py -t x.x.x.x -e /path/to/results.txt
+
+Retrieve IP Geolocation for many targets
+
+    ./ip2geolocation.py -T /path/to/targets/targets.txt
+
+Retrieve IP Geolocation for many targets and export results to xml
+
+    ./ip2geolocation.py -T /path/to/targets/targets.txt --xml /path/to/results.xml
+
+Do not print results to terminal
+
+    ./ip2geolocation.py -m -e /path/to/results.txt --noprint
+
+
+
+
+git clone https://github.com/HatBashBR/HatCloud
+
+git clone https://github.com/mrcakil/Mrcakil.git
+
+git clone https://github.com/4L13199/LITESPAM
+
+git clone https://github.com/Cvar1984/LiteOTP
+
+git clone https://github.com/4L13199/LITESCRIPT.git
+
+Tool-X
+apt update
+pkg install git
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+cd Tool-X
+chmod +x install.aex
+sh install.aex if not work than type ./install.aex
